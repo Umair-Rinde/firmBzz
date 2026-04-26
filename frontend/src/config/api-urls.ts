06e6a48d0dfbd7c9ff73b0244b3da92b2c05b18e
Umair-Rinde <rindeumair@gmail.com>
2026-04-26 15:06:45 +0530

    server api fix

--- frontend/src/config/api-urls.ts
+++ frontend/src/config/api-urls.ts
@@ -1,8 +1,8 @@
 /** Hardcoded API base until Vercel/env wiring is finalized */
-// const RAW_API = "https://umairnew.pythonanywhere.com/api";
-// const RAW_ORIGIN = "https://umairnew.pythonanywhere.com";
-const RAW_API = "http://127.0.0.1:8000/api";
-const RAW_ORIGIN = "http://127.0.0.1:8000";
+const RAW_API = "https://umairnew.pythonanywhere.com/api";
+const RAW_ORIGIN = "https://umairnew.pythonanywhere.com";
+// const RAW_API = "http://127.0.0.1:8000/api";
+// const RAW_ORIGIN = "http://127.0.0.1:8000";
 
 
 export const API_URL = RAW_API.replace(/\/$/, "");
