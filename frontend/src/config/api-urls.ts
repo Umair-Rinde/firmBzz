53437ea5f7762c7e8779501a3d60060f628c62e6
Umair-Rinde <rindeumair@gmail.com>
2026-05-02 14:18:08 +0530

    new build

--- frontend/src/config/api-urls.ts
+++ frontend/src/config/api-urls.ts
@@ -1,10 +1,8 @@
 /** Hardcoded API base until Vercel/env wiring is finalized */
 
-//   const RAW_API = "https://anmolnexus.com/api";
-//   const RAW_ORIGIN = "https://anmolnexus.com";
-const RAW_API = "http://127.0.0.1:8000/api";
-const RAW_ORIGIN = "http://127.0.0.1:8000";
-const API_URL = RAW_API.replace(/\/$/, "");
+  const RAW_API = "https://anmolnexus.com/api";
+  const RAW_ORIGIN = "https://anmolnexus.com";
+  const API_URL = RAW_API.replace(/\/$/, "");
   const API_ORIGIN = RAW_ORIGIN.replace(/\/$/, "");
 
 export { API_URL, API_ORIGIN };
